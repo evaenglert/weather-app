efe4b5b20fcea835270bf4222bed880d0ed0c359
Franciska Englert <englert.franciska@gmail.com>
2023-01-19 08:57:40 +0000

    Simple form to send location for weather

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 console.log("Hello world");
 
-function getWeatherFromLocation(location, YOUR_API_KEY) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${YOUR_API_KEY}`)
-    .then(function(response) {
-      return response.json()
-    })
+async function getWeatherFromLocation(location) {
+  YOUR_API_KEY = 'f8f205b09395cd5cae91d8cab87adfca'
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${YOUR_API_KEY}`);
+  response_json = await response.json();
+  console.log(response_json.main.temp);
 }
 
-async function getWeatherFromLocation(location, YOUR_API_KEY) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${YOUR_API_KEY}`)
-  return response.json()
-
-}
-
-// async function formatWeatherData(response) {
-//   response
-// }
+var search_button = document.querySelector(".search-btn");
+search_button.addEventListener('click', () => {
+  var location = document.querySelector("#location").value;
+  getWeatherFromLocation(location);
+})
